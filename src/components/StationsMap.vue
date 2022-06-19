08b1cc30c35f255c191aa65214e03b087e147ee7
J. Verstraete <jeroen.verstraete@dxcfds.com>
2022-06-19 13:47:05 +0200

    First icon created

--- src/components/StationsMap.vue
+++ src/components/StationsMap.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <l-map style="height: 300px" :zoom="zoom" :center="center">
+    <l-map style="height: 50vh" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="markerLatLng" :icon="icon"/>
     </l-map>
 
   </div>
 </template>
 
 <script lang="ts">
-
+import L from 'leaflet';
 import { Component, Vue } from 'vue-property-decorator';
 import { LMap, LMarker, LTileLayer } from 'vue2-leaflet';
 
@@ -21,13 +22,15 @@ import { LMap, LMarker, LTileLayer } from 'vue2-leaflet';
 })
 export default class StationsMap extends Vue {
   url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-
   attribution = '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors';
-
   zoom = 15;
-
   center = [51.505, -0.159];
-
   markerLatLng = [51.504, -0.159];
+
+  icon = L.icon({
+    iconUrl: require('@/assets/logo.png'),
+    iconSize: [20, 20],
+    iconAnchor: [10, 10]
+  });
 }
 </script>
