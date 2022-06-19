e53a761c4b8c230f8ad145dec45e1038a66706eb
J. Verstraete <jeroen.verstraete@dxcfds.com>
2022-06-19 14:51:58 +0200

    Header update

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,14 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src=" require('@/assets/logo-velo.svg')"
+
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h2>
+          Velo-finder
+        </h2>
       </div>
 
       <v-spacer></v-spacer>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,29 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#ff5a00',
+        primaryAccent1: '#4B7284',
+        primaryAccent2: '#728F9B',
+        primaryAccent3: '#B7C9CD',
+        secondary: '#38373A',
+        secondaryAccent1: '#6B6C6E',
+        secondaryAccent2: '#98999B',
+        secondaryAccent3: '#C2C3C4',
+        success: '#1A5924',
+        successAccent1: '#4AA832',
+        successAccent2: '#9FCB8A',
+        error: '#C83C22',
+        errorAccent1: '#E8754C',
+        errorAccent2: '#EBA386',
+        info: '#075E8D',
+        infoAccent1: '#3F7A9C',
+        infoAccent2: '#6098B6',
+
+        white: '#fff',
+      },
+    },
+  },
 });
