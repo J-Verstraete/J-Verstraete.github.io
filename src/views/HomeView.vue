fd92a31beea18b75bea76a9266539d2d1ad10961
J. Verstraete <jeroen.verstraete@dxcfds.com>
2022-06-19 12:31:57 +0200

    First axios-call

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,31 @@
 <template>
-  <hello-world />
+  <div>
+    v-
+    {{ stations }}
+    <hello-world/>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios'; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  stations:object[] = [];
+
+  mounted() {
+    axios.get('http://api.citybik.es/v2/networks/velo-antwerpen')
+      .then((resp) => resp.data)
+      .then((data) => data.network)
+      .then((network) => {
+        network.stations.forEach((station: object) => this.stations.push(station));
+      });
+  }
+}
 </script>
